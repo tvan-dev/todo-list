a808ab2575cc1c8b2e343ad34461bee2a1869e77
Tvan <hothithanhvan.dev@gmail.com>
2025-04-03 23:47:37 +0700

    2. Feat: Edit Task

--- main.js
+++ main.js
@@ -9,19 +9,20 @@ const input = $("#todo-input")
 const submitBtn = $(".submit-btn")
 const taskList = $(".task-list")
 const taskItem = $(".task-item")
-;
 
-// ngăn chặn mất focus khi click vào nút submit đã thay bằng input.focus()
+// ngăn chặn mất focus khi click vào nút submit or thay bằng input.focus()
 // submitBtn.onmousedown = function (e) {
 //     e.preventDefault()
 // }
+
+//1. render tasks list
 function renderTask() {
     const htmls = tasks.map(task => {
         return `
         <li class="task-item ${task.completed ? 'completed' : ''}">
             <span class="task-title">${task.title}</span>
             <div class="task-action">
-                <button class="task-btn edit">Edit</button>
+                <button class="task-btn edit" onclick="edit(${task.id})">Edit</button>
                 <button class="task-btn done">${task.completed ? "Completed" : "Done"} </button>
                 <button class="task-btn delete">Delete</button>
             </div>
@@ -30,31 +31,54 @@ function renderTask() {
     
     taskList.innerHTML = htmls.join("")
 }
+renderTask()
 
-form.onsubmit = function (e) {
-    e.preventDefault();
-
-    const taskName = input.value.trim()
+//2. kiểm tra task đầu vào hợp lệ 
+function isValidTask(taskName) {
     const trueName = tasks.find(task => task.title.toLowerCase() === taskName.toLowerCase())
     
     if (!taskName) {
-        return alert("Please enter your task!")
+        alert("Please enter your task!")
+        return false
         
     }
     
     if (trueName) {
         alert("Task already exists!")
         input.value = ""
-        return
+        return false
     }
-
-    tasks.push({
-        title: taskName,
-        completed: false,
-    })
-    input.value = ""
-    input.focus()
-    renderTask()
+    return true
 }
 
+// 3. thêm task
+form.onsubmit =  function (e) {
+    e.preventDefault();
 
+    const taskName = input.value.trim()
+
+    if(isValidTask(taskName)) {
+        console.log("hello");
+        tasks.push({
+            id: tasks.length + 1,
+            title: taskName,
+            completed: false,
+        })
+        input.value = ""
+        renderTask()
+    }
+    
+}
+
+//4. chỉnh sửa task
+function edit(id) {
+    tasks.forEach(task => {
+        if(task.id === id) {
+            const taskEdited = prompt("Edit your task", task.title)
+            isValidTask(taskEdited)
+            task.title = taskEdit
+        }
+
+    })
+    renderTask()
+}
